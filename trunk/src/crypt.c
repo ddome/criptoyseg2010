9b44411d123cdec4e28aad469b72aad0db38aa40
bombax <bombax@0439184e-0cea-1462-2dda-29a681303e51>
2010-04-27 07:39:40 +0000

    2 de aes

--- trunk/src/crypt.c
+++ trunk/src/crypt.c
@@ -4,6 +4,7 @@
 #define ECB 2
 #define CFB 3
 #define OFB 4
+
 /*Las siguientes funciones devuelve 0 o NULL en caso de error. Si todo sale como planeado devuelven 1 o un valor distinto de NULL*/
 /*Prototipo de funciones para encriptar y desencriptar con AES*/
 int CryptAES(char * key,char * method,char * password,char * vi,FILE * srcFile,FILE * dstFile,imageInfo info,int type);
@@ -38,6 +39,7 @@ Start(int encrypt,int decrypt,char * key,char * method,char * alg,char * passwor
     int status=1;
     if(encrypt)
     {
+        /*Modo de encriptacion*/
         if(strncmp(alg,"aes",3)==0)
         {
 	status=CryptAES(key,method,password,vi,srcFile,dstFile,info,AES_ENCRYPT);
@@ -53,6 +55,7 @@ Start(int encrypt,int decrypt,char * key,char * method,char * alg,char * passwor
     }
     else if(decrypt)
     {
+        /*Modo de desencriptacion*/
         if(strncmp(alg,"aes",3)==0)
         {
 	status=CryptAES(key,method,password,vi,srcFile,dstFile,info,AES_DECRYPT);
@@ -74,14 +77,19 @@ Start(int encrypt,int decrypt,char * key,char * method,char * alg,char * passwor
     return status;
 }
 
+/**********************************/
+/*      Funciones para AES        */
+/**********************************/
+
 int
 CryptAES(char * key,char * method,char * password,char * vi,FILE * srcFile,FILE * dstFile,imageInfo info,int type)
 {
     int methodAux=0;
-    printf("Bien1\n");
-    methodAux=GetMethod(method);
-    printf("Bien2\n");
     int status=1;
+    
+    methodAux=GetMethod(method);
+    
+    /*Elijo el metodo a utilizar para encriptar o desencriptar con AES*/
     if(methodAux==CBC)
     {
         status=AES_CBC_crypt(srcFile,dstFile,info,key,vi,type);
@@ -109,30 +117,108 @@ CryptAES(char * key,char * method,char * password,char * vi,FILE * srcFile,FILE 
 int
 AES_CBC_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * key,char * vi,int type)
 {
-    /*AES CBC*/
+    char block[16]={0};
+    char src_block[16]={0};
+    AES_KEY pass;
+    
+    CopyHeader(srcFile,dstFile,info);
+    strncpy((char *)block,key,16);
+    
+    if( type == AES_ENCRYPT )
+        AES_set_encrypt_key((const unsigned char *)&block,128,&pass);
+    else
+        AES_set_decrypt_key((const unsigned char *)&block,128,&pass);
+
+    
+    while((fread(src_block,16*sizeof(char),1,srcFile))==1)
+    {
+        AES_cbc_encrypt((const unsigned char *)&src_block,(unsigned char *)&block,16,&pass,(unsigned char * )vi,type);
+        fwrite(&block,16*sizeof(char),1,dstFile);
+    }
+    
     return 1;
 }
 
 int
 AES_ECB_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * password,int type)
 {
-    /*AES ECB*/
+    char block[16]={0};
+    char src_block[16]={0};
+    AES_KEY pass;
+    
+    CopyHeader(srcFile,dstFile,info);
+    strncpy((char *)block,password,16);
+    
+    if( type == AES_ENCRYPT )
+        AES_set_encrypt_key((const unsigned char *)&block,128,&pass);
+    else
+        AES_set_decrypt_key((const unsigned char *)&block,128,&pass);
+
+    
+    while((fread(src_block,16*sizeof(char),1,srcFile))==1)
+    {
+        AES_ecb_encrypt((const unsigned char *)&src_block,(unsigned char *)&block,&pass,type);
+        fwrite(&block,16*sizeof(char),1,dstFile);
+    }
+    
     return 1;
 }
 
 int
 AES_CFB_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * key,char * vi,int type)
 {
-    /*AES CFB*/
+    char block[16]={0};
+    char src_block[16]={0};
+    AES_KEY pass;
+    int num=15;
+    
+    CopyHeader(srcFile,dstFile,info);
+    strncpy((char *)block,key,16);
+    printf("AES CFB Tipo: %d\n",type);
+    if( type == AES_ENCRYPT )
+        AES_set_encrypt_key((const unsigned char *)&block,128,&pass);
+    else
+        AES_set_decrypt_key((const unsigned char *)&block,128,&pass);
+
+    
+    while((fread(src_block,16*sizeof(char),1,srcFile))==1)
+    {
+        AES_cfb128_encrypt(&src_block,&block,num,&pass,vi,&num,type);
+        fwrite(&block,16*sizeof(char),1,dstFile);
+    }
+    
     return 1;
 }
 
 int
 AES_OFB_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * key,char * vi,int type)
 {
-    /*AES OFB*/
+    unsigned char block[16]={0};
+    unsigned char src_block[16]={0};
+    AES_KEY pass;
+    int num=15;
+    
+    CopyHeader(srcFile,dstFile,info);
+    strncpy((char *)block,key,16);
+    printf("AES OFB Tipo: %d\n",type);
+    if( type == AES_ENCRYPT )
+        AES_set_encrypt_key((const unsigned char *)&block,128,&pass);
+    else
+        AES_set_decrypt_key((const unsigned char *)&block,128,&pass);
+
+    
+    while((fread(src_block,16*sizeof(char),1,srcFile))==1)
+    {
+        AES_ofb128_encrypt((const unsigned char *)&src_block,(unsigned char *)&block,16,&pass,(unsigned char * )vi,&num);
+        fwrite(&block,16*sizeof(char),1,dstFile);
+    }
+    printf("aca\n");
     return 1;
 }
+
+/**********************************/
+/*      Funciones para DES        */
+/**********************************/
 
 int
 CryptDES(char * key,char * method,char * password,char * vi,FILE * srcFile,FILE * dstFile,imageInfo info,int type)
@@ -142,6 +228,7 @@ CryptDES(char * key,char * method,char * password,char * vi,FILE * srcFile,FILE 
     
     methodAux=GetMethod(method);
     
+    /*Elijo el metodo a utilizar para encriptar o desencriptar con DES*/
     if(methodAux==CBC)
     {
         status=DES_CBC_crypt(srcFile,dstFile,info,key,vi,type);
@@ -174,7 +261,8 @@ DES_ECB_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * password,int t
     CopyHeader(srcFile,dstFile,info);
     DES_key_schedule pass;
     strncpy((char *)block,password,8);
-    DES_set_key(&block,&pass);
+    DES_set_odd_parity(&block);
+    DES_set_key_checked(&block,&pass);
     
     while((fread(src_block,8*sizeof(char),1,srcFile))==1)
     {
@@ -187,14 +275,15 @@ DES_ECB_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * password,int t
 int
 DES_CBC_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * key,char * vi,int type)
 {
-    unsigned char block[8];
-    unsigned char src_block[8];
+    unsigned char block[8]={0};
+    unsigned char src_block[8]={0};
     DES_cblock vi_block;
     CopyHeader(srcFile,dstFile,info);
     DES_key_schedule pass;
     strncpy((char *)block,key,8);
     strncpy((char *)vi_block,vi,8);
-    DES_set_key(&block,&pass);
+    DES_set_odd_parity(&block);
+    DES_set_key_checked(&block,&pass);
     
     while((fread(src_block,8*sizeof(char),1,srcFile))==1)
     {
@@ -207,14 +296,15 @@ DES_CBC_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * key,char * vi,
 int
 DES_CFB_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * key,char * vi,int type)
 {
-    unsigned char block[8];
-    unsigned char src_block[8];
+    unsigned char block[8]={0};
+    unsigned char src_block[8]={0};
     DES_cblock vi_block;
     CopyHeader(srcFile,dstFile,info);
     DES_key_schedule pass;
     strncpy((char *)block,key,8);
     strncpy((char *)vi_block,vi,8);
-    DES_set_key(&block,&pass);
+    DES_set_odd_parity(&block);
+    DES_set_key_checked(&block,&pass);
     
     while((fread(src_block,8*sizeof(char),1,srcFile))==1)
     {
@@ -227,15 +317,15 @@ DES_CFB_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * key,char * vi,
 int
 DES_OFB_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * key,char * vi,int type)
 {
-    /*NO ANDA*/
-    unsigned char block[8];
-    unsigned char src_block[8];
+    unsigned char block[8]={0};
+    unsigned char src_block[8]={0};
     DES_cblock vi_block;
     CopyHeader(srcFile,dstFile,info);
     DES_key_schedule pass;
     strncpy((char *)block,key,8);
     strncpy((char *)vi_block,vi,8);
-    DES_set_key(&block,&pass);
+    DES_set_odd_parity(&block);
+    DES_set_key_checked(&block,&pass);
 
     while((fread(src_block,8*sizeof(char),1,srcFile))==1)
     {
@@ -246,6 +336,9 @@ DES_OFB_crypt(FILE * srcFile,FILE * dstFile,imageInfo info,char * key,char * vi,
     return 1;
 }
 
+/**********************************/
+/*      Funciones auxiliares      */
+/**********************************/
 
 int
 GetMethod(char * method)
@@ -295,21 +388,3 @@ CopyHeader(FILE * srcFile,FILE * dstFile,imageInfo info)
     fwrite(aux,54,1,dstFile);
     free(aux);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
